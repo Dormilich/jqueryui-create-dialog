14f8e5788f695b9d58cb988781175de4d21a2bc6
Bertold von Dormilich <Dormilich@netscape.net>
2015-06-23 21:12:24 +0200

    reorganising tests

--- test/test.js
+++ test/test.js
@@ -20,49 +20,49 @@
       throws(block, [expected], [message])
   */
 
-    module('formDialog: basics', {
+    QUnit.module('formDialog: basics', {
         // This will run before each test in this module.
         setup: function() {
             this.elems = $('#qunit-fixture').children();
         }
     });
 
-    test('is chainable', function() {
-        expect(1);
+    QUnit.test('is chainable', function (assert) {
+        assert.expect(1);
         // Not a bad test to run on collection methods.
-        strictEqual(this.elems.formDialog(), this.elems, 'should be chainable');
+        assert.strictEqual(this.elems.formDialog(), this.elems, 'should be chainable');
     });
 
-    module('formDialog: buttons');
+    QUnit.module('formDialog: buttons');
 
-    test('button properties', function () {
-        expect(3);
+    QUnit.test('button properties', function (assert) {
+        assert.expect(3);
 
         var element = $('<div></div>').formDialog(),
             btn = element.dialog('widget').find('.ui-dialog-buttonpane button');
         
-        equal(btn.length, 2, 'number of buttons');
-        equal(btn.eq(0).text(), 'Save', 'text of save button');
-        equal(btn.eq(1).text(), 'Cancel', 'text of cancel button');
+        assert.equal(btn.length, 2, 'number of buttons');
+        assert.equal(btn.eq(0).text(), 'Save', 'text of save button');
+        assert.equal(btn.eq(1).text(), 'Cancel', 'text of cancel button');
     });
 
-    test('button label', function () {
-        expect(1);
+    QUnit.test('button label', function (assert) {
+        assert.expect(1);
 
         var element = $('<div></div>').formDialog({ actionLabel: 'FizzBuzz' }),
             btn = element.dialog('widget').find('.ui-dialog-buttonpane button');
         
-        equal(btn.eq(0).text(), 'FizzBuzz', 'custom text of save button');
+        assert.equal(btn.eq(0).text(), 'FizzBuzz', 'custom text of save button');
     });
 
-    test('translate label', function () {
-        expect(2);
+    QUnit.test('translate label', function (assert) {
+        assert.expect(2);
 
         var element = $('<div></div>').formDialog({ language: 'de' }),
             btn = element.dialog('widget').find('.ui-dialog-buttonpane button');
         
-        equal(btn.eq(0).text(), 'Speichern', 'translated text of save button');
-        equal(btn.eq(1).text(), 'Abbrechen', 'translated text of cancel button');
+        assert.equal(btn.eq(0).text(), 'Speichern', 'translated text of save button');
+        assert.equal(btn.eq(1).text(), 'Abbrechen', 'translated text of cancel button');
     });
 
 }(jQuery));
